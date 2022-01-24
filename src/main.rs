eb724db267f671bde34dcb404189dcf564f5bc4b
RubixDev <silas.groh@t-online.de>
2022-01-24 21:28:12 +0100

    Improve user interface

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,34 @@ mod parser;
 mod nodes;
 mod interpreter;
 
-use std::io::{BufRead, Write};
 use lexer::Lexer;
 use parser::Parser;
 use interpreter::Interpreter;
+use rustyline::{Editor, error::ReadlineError};
 
 fn main() {
+    let mut rl = Editor::<()>::new();
     loop {
-        print!(">>> ");
-        std::io::stdout().flush().unwrap();
-        let line = std::io::stdin().lock().lines().next().unwrap().unwrap();
-        if line.chars().all(|char| [' ', '\t'].contains(&char)) { continue; }
+        let line = rl.readline("> ");
+        match line {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                if line.chars().all(|char| [' ', '\t'].contains(&char)) { continue; }
 
-        let start = std::time::Instant::now();
+                let start = std::time::Instant::now();
 
-        let mut lexer = Lexer::new(&line);
-        let tokens = lexer.scan();
-        // println!("{:?}", tokens);
+                let mut lexer = Lexer::new(&line);
+                let tokens = lexer.scan();
 
-        let mut parser = Parser::new(tokens);
-        let nodes = parser.parse();
+                let mut parser = Parser::new(tokens);
+                let nodes = parser.parse();
 
-        let interpreter = Interpreter::new(nodes);
-        interpreter.run();
-        println!("\x1b[90m[{:?}]\x1b[0m", start.elapsed());
+                let interpreter = Interpreter::new(nodes);
+                interpreter.run();
+                println!("\x1b[90m[{:?}]\x1b[0m", start.elapsed());
+            },
+            Err(ReadlineError::Eof) => break,
+            Err(_) => std::process::exit(1),
+        }
     }
 }
